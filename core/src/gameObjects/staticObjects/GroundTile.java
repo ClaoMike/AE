d2233c61689fc1323e38f09ce66754974102f143
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2023-02-08 16:09:46 +0000

    Setup GroundCube class

--- core/src/gameObjects/staticObjects/GroundTile.java
+++ core/src/gameObjects/staticObjects/GroundTile.java
@@ -1,11 +1,43 @@
 package gameObjects.staticObjects;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+
+import helpers.GameInfo;
 
 public class GroundTile extends Sprite {
-    private final String name = "groundCube";
+    private World world;
+    private Body body;
 
-    public GroundTile() {
+    public GroundTile(World world) {
+        super(new Texture("groundCube.png"));
+        this.world = world;
+    }
 
+    void createBody() {
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.StaticBody;
+        bodyDef.position.set((getX() + getWidth()/2) / GameInfo.PPM, (getY() + getHeight()/2)/GameInfo.PPM);
+
+        body = world.createBody(bodyDef);
+
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(getWidth()/2/GameInfo.PPM, getHeight()/2/GameInfo.PPM);
+
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = shape;
+
+        Fixture fixture = body.createFixture(fixtureDef);
+    }
+
+    public void setSpritePosition(float x, float y) {
+        setPosition(x, y);
+        createBody();
     }
 }
