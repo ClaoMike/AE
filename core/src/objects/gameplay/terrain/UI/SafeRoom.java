4fc6a330ca80b357da8153130d040ce5d265fe08
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2023-02-22 17:28:42 +0000

    Reafctor the map

--- core/src/objects/gameplay/terrain/UI/SafeRoom.java
+++ core/src/objects/gameplay/terrain/UI/SafeRoom.java
@@ -2,66 +2,56 @@ package objects.gameplay.terrain.UI;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.physics.box2d.World;
-import com.badlogic.gdx.utils.Array;
 
 import objects.gameplay.Drawable;
 
 public class SafeRoom implements Drawable {
-    private int number_of_platforms = 1;
-    private final Array<Platform> bottomPlatform = new Array<>();
-    private BasicRoom b;
-    private final Array<Platform> topPlatform = new Array<>();
+    private final Platform bottomPlatform;
+    private BasicRoom wall;
+    private final Platform topPlatform;
 
-    public SafeRoom(SpriteBatch batch, World world, float x, float y) {
-        for (int i = 0; i < number_of_platforms; i++) {
-            Platform p = new Platform(batch, world, x, y);
-            bottomPlatform.add(p);
+    public SafeRoom(SpriteBatch batch, World world, float x, float y, boolean placeWallToTheLeft) {
+        wall = new BasicRoom(batch, world, x, y);
+        float wallSize = wall.getSize();
+        wall.dispose();
+
+        bottomPlatform = new Platform(batch, world, x, y);
+
+        y -= wallSize*2;
+
+        topPlatform = new Platform(batch, world, x, y);
+
+        if(!placeWallToTheLeft) {
+            x += topPlatform.getWidth() - wallSize;
         }
+        y += wallSize;
 
-        b = new BasicRoom(batch, world, x, y);
-        y -= b.getSize();
-        b.dispose();
-        b = new BasicRoom(batch, world, x, y);
-
-        y -= b.getSize();
-
-        for (int i = 0; i < number_of_platforms; i++) {
-            Platform p = new Platform(batch, world, x, y);
-            topPlatform.add(p);
-        }
+        wall = new BasicRoom(batch, world, x, y);
     }
 
     @Override
     public void draw() {
-        b.draw();
-        for(Platform p : bottomPlatform) {
-            p.draw();
-        }
-        for(Platform p : topPlatform) {
-            p.draw();
-        }
+        wall.draw();
+        bottomPlatform.draw();
+        topPlatform.draw();
     }
 
     @Override
     public void updatePosition() {
-        b.updatePosition();
-        for(Platform p : bottomPlatform) {
-            p.updatePosition();
-        }
-
-        for(Platform p : topPlatform) {
-            p.updatePosition();
-        }
+        wall.updatePosition();
+        bottomPlatform.updatePosition();
+        topPlatform.updatePosition();
     }
 
     @Override
     public void dispose() {
-        b.dispose();
-        for(Platform p : bottomPlatform) {
-            p.dispose();
-        }
-        for(Platform p : topPlatform) {
-            p.dispose();
-        }
+        wall.dispose();
+        bottomPlatform.dispose();
+        topPlatform.dispose();
     }
+
+    public float getSize() {
+        return bottomPlatform.getWidth();
+    }
+
 }
--- core/src/objects/gameplay/terrain/WorldTerrainGenerator.java
+++ core/src/objects/gameplay/terrain/WorldTerrainGenerator.java
@@ -1,82 +1,68 @@
 package objects.gameplay.terrain;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.World;
-
-import helpers.GameInfo;
-import objects.gameplay.AtomicObject;
 import objects.gameplay.Drawable;
 import objects.gameplay.terrain.UI.BasicRoom;
 import objects.gameplay.terrain.UI.DrawableMaze;
-import objects.gameplay.terrain.UI.Platform;
 import objects.gameplay.terrain.UI.SafeRoom;
 
 public class WorldTerrainGenerator implements Drawable {
-    private float initalX = 0;
-    private float initalY = 0;
-    private final World world;
     private final SpriteBatch batch;
-
-    private Platform startPlatform;
+    private final World world;
+    private float x;
+    private float y;
+    private SafeRoom startRoom;
     private DrawableMaze maze;
-    private Platform endPlatform;
-
-    private SafeRoom s;
+    private SafeRoom endRoom;
 
     public WorldTerrainGenerator(SpriteBatch batch, World world, float x, float y) {
         this.world = world;
         this.batch = batch;
+        this.x = x;
+        this.y = y;
 
-        s = new SafeRoom(batch, world, x, y);
-
-//        createStartPlatform();
-//        createMaze();
-//        createEndPlatform();
+        createStartPlatform();
+        createMaze();
+        createEndPlatform();
     }
 
     private void createStartPlatform() {
-        startPlatform = new Platform(batch, world, initalX, initalY);
+        startRoom = new SafeRoom(batch, world, x, y, true);
 
-        // update x and y for the maze
-        BasicRoom br = new BasicRoom(batch, world, 0, 0);
-        AtomicObject ao = new AtomicObject(GameInfo.PLAYER, 0, 0, world, BodyDef.BodyType.StaticBody, 1, false, false);
-        initalY = br.getSize() - ao.getHeight();
-        initalX = startPlatform.getWidth();
-        br.dispose();
-        ao.dispose();
+        x += startRoom.getSize();
+        BasicRoom b = new BasicRoom(batch, world, 0, 0);
+        y -= b.getSize();
+        b.dispose();
     }
 
     private void createMaze() {
-        maze = new DrawableMaze(batch, world, initalX, initalY);
+        maze = new DrawableMaze(batch, world, x, y);
 
         // update x and y for the end platform
-        initalX += maze.getWidth();
-        initalY = maze.getExitY();
+        x += maze.getWidth();
+        y = maze.getExitY() + 700;
     }
 
     private void createEndPlatform() {
-        endPlatform = new Platform(batch, world, initalX, initalY);
+        endRoom = new SafeRoom(batch, world, x, y, false);
     }
 
     public void draw() {
-        s.draw();
-//        startPlatform.draw();
-//        maze.draw();
-//        endPlatform.draw();
+        startRoom.draw();
+        maze.draw();
+        endRoom.draw();
     }
 
     public void updatePosition() {
-        s.updatePosition();
-//        startPlatform.updatePosition();
-//        maze.updatePosition();
-//        endPlatform.updatePosition();
+        startRoom.updatePosition();
+        maze.updatePosition();
+        endRoom.updatePosition();
     }
 
     public void dispose() {
-        s.dispose();
-//        startPlatform.dispose();
-//        maze.dispose();
-//        endPlatform.dispose();
+        startRoom.dispose();
+        maze.dispose();
+        endRoom.dispose();
     }
 }
